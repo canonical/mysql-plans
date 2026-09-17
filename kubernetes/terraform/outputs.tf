2a1ce4da64ba4a39a57a4ed33df6af454cc46a0d
Soundarya03 <soundarya1103@gmail.com>
2026-09-17 19:57:44 +0530

    refactor: address review comments

--- kubernetes/terraform/outputs.tf
+++ kubernetes/terraform/outputs.tf
@@ -5,14 +5,14 @@ output "app_names" {
   description = "Names of of all deployed applications."
   value = {
     mysql_server = module.mysql_server.app_name
-    mysql_router = var.router_enabled ? module.mysql_router[0].app_name : null
+    mysql_router = length(module.mysql_router) > 0 ? module.mysql_router[0].app_name : null
   }
 }
 
 output "provides" {
   description = "Map of all the provided endpoints"
   value = {
-    database = var.router_enabled ? module.mysql_router[0].provides.database : module.mysql_server.provides.database
+    database = length(module.mysql_router) > 0 ? module.mysql_router[0].provides.database : module.mysql_server.provides.database
   }
 }
 
